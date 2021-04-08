eb4b3c3a4a4f674ba738db6b8f7d0d7fe2c30b0a
Martin Hoffmann <maddinpsycho@gmail.com>
2021-04-08 13:16:53 +0200

    bugfix startup flicker

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -4,10 +4,19 @@ import { BoardProps } from "boardgame.io/react";
 import { SpiritIslandState } from "game/Game";
 import { IslandArea } from "./IslandArea";
 
-export class SpiritIslandBoard extends React.Component<BoardProps<SpiritIslandState>, any> {
+export class SpiritIslandBoard extends React.Component<BoardProps<SpiritIslandState>, {loading:boolean}> {
+    constructor(props:any){
+        super(props)
+        this.state={loading:true}
+    }
+    componentDidMount(){
+        //avoid startup flicker, for one second
+        window.setTimeout(()=>this.setState({loading:false}),1000);
+    }
     render() {
-        if (!this.props.matchData) {
-            return (<div>"this.props.matchData is not defined."</div>);
+        if(this.state.loading){
+            //show loading compontne to avoid startup flicker
+            return (<div>connecting..</div>)
         }
         //mapping from id to name comes from this.props.matchData  
         return (
