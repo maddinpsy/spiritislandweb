94b5a4ec4c93fd2d887f1ea3022f0b084747b1ad
Martin Hoffmann <maddinpsycho@gmail.com>
2021-04-26 22:51:10 +0200

    add some comments

--- src/components/PhaseMain/Tokens/LandTokens/index.tsx
+++ src/components/PhaseMain/Tokens/LandTokens/index.tsx
@@ -176,6 +176,7 @@ export class LandTokens extends React.Component<LandTokensProps>{
 
     render() {
         const bt = this.props.boardTokens;
+        //for each land
         return bt.lands.map(l => {
             //get token position
             const polyAbs = LandOutline[bt.boardName][l.landNumber].map(point => {
@@ -184,22 +185,28 @@ export class LandTokens extends React.Component<LandTokensProps>{
             });
             let tokenSizes = tokenContainerSizes[0];
             let tokensAndNew = Array.from(l.tokens);
+            //add one extra token for the newTokenButton
             tokensAndNew.push({ count: 1, tokenType: "Badlands" });
             let tokenPos = this.getTokenPosition(tokensAndNew, polyAbs, tokenSizes);
+            // try to fit tokens into the land, with different tokenSizes
             for (let i = 1; i < tokenContainerSizes.length; i++) {
+                //break the loop and keet the current tokenSize
                 if (tokenPos) {
                     break;
                 }
                 tokenSizes = tokenContainerSizes[i];
                 tokenPos = this.getTokenPosition(tokensAndNew, polyAbs, tokenSizes);
             }
+            //if all token sizes have been tried, show an error message.
             if (!tokenPos) {
                 console.log("Could not fit tokens (" + tokensAndNew.map(t => t.count + " " + t.tokenType) + ") into land: " + bt.boardName + l.landNumber);
                 return (<div style={{ left: polyAbs[0][0], top: polyAbs[0][1], position: "absolute" }}>ERROR</div>);
             }
+            //make list constant, to use it in map loop
             const cTokenPos = tokenPos;
             //generate token elements
             const tokens = l.tokens.map((t, idx) => {
+                //prepare per element style with absolut position
                 let customStyle: React.CSSProperties = {};
                 customStyle.left = cTokenPos[idx].left;
                 customStyle.top = cTokenPos[idx].top;
@@ -209,11 +216,16 @@ export class LandTokens extends React.Component<LandTokensProps>{
                 return <TokenOnBoard token={t} selected={isSelected}
                     id={bt.boardName + l.landNumber + t.tokenType}
                     key={bt.boardName + l.landNumber + t.tokenType}
+                    //callbacks for increse and decrease
                     onIncrease={() => this.props.onIncreaseToken(bt.boardName, l.landNumber, t.tokenType)}
                     onDecrease={() => this.props.onDecreaseToken(bt.boardName, l.landNumber, t.tokenType)}
+                    //class based on the calculated size
                     className={classnames(style.Tokens__token, tokenSizes.classname)}
+                    //width of the increase/decrese buttons
                     buttonWidth={tokenSizes.buttonWidth}
+                    //absolut position from calculated positions
                     style={customStyle}
+                    //toggle increase/decrease buttons
                     onClick={() => {
                         if (isSelected) {
                             this.props.onSelectToken(undefined);
@@ -221,11 +233,12 @@ export class LandTokens extends React.Component<LandTokensProps>{
                             this.props.onSelectToken({ board: bt.boardName, land: l.landNumber, token: t.tokenType });
                         }
                     }} />;
-            });
+            }); //end for each token
             return (
                 <div id={"LandTokens_" + bt.boardName + l.landNumber} key={bt.boardName + l.landNumber}>
                     {tokens}
                     <AddNewTokenButton
+                        //use extra added tokenpos
                         position={cTokenPos[cTokenPos.length - 1]}
                         className={tokenSizes.classname}
                         showDialog={this.props.showDialog}
@@ -234,6 +247,6 @@ export class LandTokens extends React.Component<LandTokensProps>{
                     />
                 </div>
             );
-        });
+        }); //end for each lands
     }
 }
